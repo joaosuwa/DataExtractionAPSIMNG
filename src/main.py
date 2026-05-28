d3a7f31f33ea4e12652284f39604e16b70da12cc
joaosuwa <joao.suwa10@gmail.com>
2026-05-28 13:31:12 -0300

    .

--- src/main.py
+++ src/main.py
@@ -9,15 +9,12 @@ simulation_dir = f'{apsim_dir}/Tests/Simulation/ZMQ-Sync/DataExtraction/wheat_so
 apsim = test_proto2(apsim_dir, simulation_dir)
 rec = {"iter":[], "mem":[], "time":[]}
 
-for i in range(10):
-    start_time = time.time()
-    poll_zmq2(apsim['apsim_socket'])
-    proc = psutil.Process(apsim['process'].pid)
-    mem_info = proc.memory_info().rss
-    elapsed_time = time.time() - start_time
-    rec["iter"].append(i + 1)
-    rec["mem"].append(mem_info)
-    rec["time"].append(elapsed_time)
+start_time = time.time()
+poll_zmq2(apsim['apsim_socket'])
+proc = psutil.Process(apsim['process'].pid)
+mem_info = proc.memory_info().rss
+elapsed_time = time.time() - start_time
+rec["time"].append(elapsed_time)
 
 close_zmq2(apsim)
 print(pd.DataFrame(rec))
